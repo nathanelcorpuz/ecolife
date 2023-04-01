fa7dbe9cee75617fd6da3b89af157e899bd14d11
natcorpuz <natcorpuz8@gmail.com>
2023-04-01 10:23:20 +0800

    init,

---
+++ src/lib/server/models/Test.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+
+const testSchema = new Schema({
+	name: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
+
+const Test = models.Test || model("Test", testSchema);
+
+export default Test;
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectMongo from "@/lib/server/services/connectMongo";
+import Test from "@/lib/server/models/test";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+export default async function handler(req, res) {
+	try {
+		console.log("CONNECTING TO MONGO");
+		await connectMongo();
+		console.log("CONNECTED TO MONGO");
+
+		console.log("CREATING DOCUMENT");
+		const test = await Test.create(req.body);
+		console.log("CREATED DOCUMENT");
+
+		res.json({ test });
+	} catch (error) {
+		console.log(error);
+		res.json({ error });
+	}
 }
