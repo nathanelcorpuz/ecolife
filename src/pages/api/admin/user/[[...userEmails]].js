3782633aeb127659ebf873a2c5792fd72a273264
natdlc <nathanelcorpuz@gmail.com>
2023-04-03 12:33:21 +0800

    edit user api implementation

--- src/pages/api/admin/user/[[...userEmails]].js
+++ src/pages/api/admin/user/[[...userEmails]].js
@@ -1,4 +1,5 @@
 import createUser from "@/lib/server/helpers/admin/user/createUser";
+import editUser from "@/lib/server/helpers/admin/user/editUser";
 import getUsers from "@/lib/server/helpers/admin/user/getUsers";
 import connectMongo from "@/lib/server/services/connectMongo";
 
@@ -13,7 +14,7 @@ export default async function handler(req, res) {
 		return;
 	}
 	if (req.method === "PUT") {
-		res.status(200).json({ query: req.query, body: req.body });
+		await editUser(req, res);
 		return;
 	}
 	if (req.method === "DELETE") {
