b856a9cbd0605cb1696211ad646c7049e016aa5a
natdlc <nathanelcorpuz@gmail.com>
2023-04-06 18:51:23 +0800

    blog public API

--- src/pages/api/public/blog/[slug].js
+++ src/pages/api/public/blog/[slug].js
@@ -5,16 +5,13 @@ export default async function handler(req, res) {
 	if (req.method !== "GET") {
 		return res.status(405).json({ success: false });
 	}
+
 	try {
 		await connectMongo();
-		const { skip = 0 } = req.query;
+		const { slug } = req.query;
+		const article = await Article.findOne({ slug });
 
-		const products = await Article.find({ type: "popular" })
-			.sort({ dateCreated: -1 })
-			.skip(parseInt(skip))
-			.limit(3);
-
-		res.status(200).json({ success: true, products });
+		res.status(200).json({ success: true, article });
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({ success: false });
