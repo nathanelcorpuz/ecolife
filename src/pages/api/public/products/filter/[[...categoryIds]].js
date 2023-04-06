cb5d1f228608ce94350dff9c50235046e3dc48c6
natdlc <nathanelcorpuz@gmail.com>
2023-04-06 14:27:23 +0800

    filter products by category API implementation-refactor

--- src/pages/api/public/products/filter/[[...categoryIds]].js
+++ src/pages/api/public/products/filter/[[...categoryIds]].js
@@ -3,7 +3,6 @@ import connectMongo from "@/lib/server/services/connectMongo";
 import { isValidObjectId } from "mongoose";
 
 const PAGE_SIZE = 4;
-
 export default async function handler(req, res) {
 	if (req.method !== "GET") {
 		return res.status(405).json({ success: false });
@@ -11,10 +10,8 @@ export default async function handler(req, res) {
 
 	try {
 		await connectMongo();
-
 		const { categoryIds } = req.query;
 		const { page = 1 } = req.query;
-
 		let query = {};
 		if (categoryIds && Array.isArray(categoryIds) && categoryIds.length > 0) {
 			const validCategoryIds = categoryIds.filter(isValidObjectId);
@@ -25,15 +22,11 @@ export default async function handler(req, res) {
 				],
 			};
 		}
-
 		const count = await Product.countDocuments(query);
-
 		const totalPages = Math.ceil(count / PAGE_SIZE);
-
 		const products = await Product.find(query)
 			.skip((page - 1) * PAGE_SIZE)
 			.limit(PAGE_SIZE);
-
 		res.json({ totalPages, products });
 	} catch (error) {
 		console.error(error);
