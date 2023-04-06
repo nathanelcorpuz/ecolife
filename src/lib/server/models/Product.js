f062fe028a9974c4c932c17e4615d257d93d2697
natdlc <nathanelcorpuz@gmail.com>
2023-04-06 11:06:46 +0800

    model update: add type to product for featured and bestseller sections

--- src/lib/server/models/Product.js
+++ src/lib/server/models/Product.js
@@ -3,57 +3,26 @@ import mongoose from "mongoose";
 const { Schema, model, models } = mongoose;
 
 const productSchema = new Schema({
-	optionIds: [
+	type: [
 		{
-			type: Schema.Types.ObjectId,
-			ref: "Option",
+			type: String,
+			enum: ["normal", "featured", "bestseller"],
+			default: "normal",
 		},
 	],
-	categoryIds: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Category",
-		},
-	],
-	subCategoryIds: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "SubCategory",
-		},
-	],
-	title: {
-		type: String,
-		required: true,
-	},
-	slug: {
-		type: String,
-		required: true,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	salePrice: {
-		type: Number,
-		required: true,
-	},
-	stock: {
-		type: Number,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
+	optionIds: [{ type: Schema.Types.ObjectId, ref: "Option" }],
+	categoryIds: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+	subCategoryIds: [{ type: Schema.Types.ObjectId, ref: "SubCategory" }],
+	title: { type: String, required: true },
+	slug: { type: String, required: true },
+	price: { type: Number, required: true },
+	salePrice: { type: Number, required: true },
+	stock: { type: Number, required: true },
+	description: { type: String, required: true },
 	isActive: { type: Boolean, default: false },
 	isDeleted: { type: Boolean, default: false },
 	imageUrls: [String],
-	reviewIds: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Review",
-		},
-	],
+	reviewIds: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 	dateCreated: { type: Date, default: Date.now },
 });
 
