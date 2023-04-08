f6ebefde46e52d2abba5abf083933031275bf8ed
natdlc <nathanelcorpuz@gmail.com>
2023-04-09 06:31:14 +0800

    change password customer API

--- src/pages/api/public/user/change-password.js
+++ src/pages/api/public/user/change-password.js
@@ -1,4 +1,7 @@
+import RefreshToken from "@/lib/server/models/RefreshToken";
+import User from "@/lib/server/models/User";
 import verifyAccessToken from "@/lib/server/services/verifyAccessToken";
+import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
 	if (req.method !== "PUT") {
@@ -12,7 +15,23 @@ export default async function handler(req, res) {
 		}
 
 		const { password, newPassword } = req.body;
-		// WIP
+
+		const user = await User.findById(verification.userId);
+
+		await bcrypt.compare(password, user.password);
+
+		const hashedNewPassword = await bcrypt.hash(newPassword, +process.env.SALT);
+
+		await User.findByIdAndUpdate(verification.userId, {
+			password: hashedNewPassword,
+		});
+
+		await RefreshToken.updateMany(
+			{ userId: verification.userId },
+			{ $set: { isValid: false } }
+		);
+
+		return res.status(200).json({ message: "Password changed" });
 	} catch (error) {
 		console.error(error);
 		return res.status(500).json({ message: "Internal Server Error" });
