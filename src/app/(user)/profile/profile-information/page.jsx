a08cf73405e0f22a2304a1ff7a065433a74ccc95
natdlc <nathanelcorpuz@gmail.com>
2023-04-22 17:35:37 +0800

    wip profile page - profile information

--- src/app/(user)/profile/profile-information/page.jsx
+++ src/app/(user)/profile/profile-information/page.jsx
@@ -1,7 +1,12 @@
+import Input from "@/components/common/inputs/Input";
+
 export default function Page() {
 	return (
-		<main>
-			<h1>Profile Information</h1>
+		<main className="max-w-[600px] flex flex-col gap-5">
+			<h1 className="text-4xl font-bold pb-4">Profile Information</h1>
+			<Input label="Name" />
+			<Input label="Email Address" />
+			<Input label="Contact Number" />
 		</main>
 	);
 }
