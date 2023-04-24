c0c52f01e4fdc4626c46ebe8f4e27989bb537771
natdlc <nathanelcorpuz@gmail.com>
2023-04-24 14:39:57 +0800

    delete pages api implementation

--- src/app/api/protected/checkout/index.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-	if (req.method !== "GET") {
-		res.status(404).json({ errorMessage: "Request not found" });
-		return;
-	}
-	res.status(200).json({ success: true });
-}
--- src/app/api/protected/checkout/paypal.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-	if (req.method !== "POST") {
-		res.status(404).json({ errorMessage: "Request not found" });
-		return;
-	}
-	res.status(200).json({ success: true });
-}
